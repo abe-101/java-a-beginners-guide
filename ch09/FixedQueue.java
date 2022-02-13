a8aac90b0a87fe5bd79a75604384d340fdf34b6d
Abe <abe@habet.dev>
2022-02-13 10:51:28 -0500

    Fixed queue class

---
+++ ch09/FixedQueue.java
@@ -0,0 +1,32 @@
+// A fixed-sized queue class for characters.
+public class FixedQueue implements ICharQ {
+    private char[] q; // this array holds the queue
+    private int putloc, getloc; // the put and get indices
+
+    // Construct an empty queue given its size.
+    public FixedQueue(int size) {
+        q = new char[size]; // alocate memory for queue
+        putloc = getloc = 0;
+    }
+
+    // Put characters into the queue.
+    public void put(char ch) 
+        throws QueueFullException {
+
+        if(putloc==q.length) 
+            throw new QueueFullException(q.length);
+
+        q[putloc++] = ch;
+    }
+
+    // Get a character from the queue
+    public char get() 
+        throws QueueEmptyException {
+
+        if(getloc == putloc) 
+           throws new QueueEmptyException();
+
+        return q[getloc++];
+    }
+}
+
